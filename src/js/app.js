23536a33ef3bb17e2e4a64729cd72b6269bf30a9
Simon Liang <no.lhr0909@gmail.com>
2015-11-21 11:29:10 -0800

    loaded box2d in as a separate script

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,11 @@ var PIXI = require('pixi.js');
 var viewportWidth = window.innerWidth;
 var viewportHeight = window.innerHeight;
 
+console.log({
+    width: viewportWidth,
+    height: viewportHeight
+});
+
 var ticker = PIXI.ticker.shared;
 ticker.autoStart = false;
 ticker.stop();
@@ -15,41 +20,30 @@ var renderer = PIXI.autoDetectRenderer(viewportWidth, viewportHeight, {
 var stage = new PIXI.Container();
 var interactionManager = renderer.plugins.interaction;
 
-var basicText = new PIXI.Text('Basic text in pixi');
+var basicText = new PIXI.Text('Basic text in pixi', {
+    stroke: "#4a1850"
+});
 basicText.x = 30;
 basicText.y = 90;
-
-stage.addChild(basicText);
-
-var style = {
-    font : 'bold italic 36px Arial',
-    fill : '#F7EDCA',
-    stroke : '#4a1850',
-    strokeThickness : 5,
-    dropShadow : true,
-    dropShadowColor : '#555555',
-    dropShadowAngle : Math.PI / 6,
-    dropShadowDistance : 6,
-    wordWrap : true,
-    wordWrapWidth : 440
-};
-
-var richText = new PIXI.Text('Rich text with a lot of options and across multiple lines',style);
-richText.x = 30;
-richText.y = 180;
-
-stage.addChild(richText);
+basicText.interactive = true;
 
 var counter = 0;
 
-console.log(interactionManager);
+var textOnClick =  function(e) {
+    basicText.text = "You have clicked " + (++counter) + " times.";
+};
+
+basicText.on('mousedown', textOnClick)
+         .on('touchstart', textOnClick);
+
+stage.addChild(basicText);
 
 // add the renderer view element to the DOM
 document.body.appendChild(renderer.view);
 
 ticker.add(function(time) {
     renderer.render(stage);
-    //console.log(interactionManager.mouse.global.x + ", " + interactionManager.mouse.global.y);
+    // console.log(renderer.plugins.interaction.mouse.global.x + ", " + renderer.plugins.interaction.mouse.global.y);
 });
 
 ticker.start();
